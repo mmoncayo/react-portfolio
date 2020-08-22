a52fbb326472a09fd28e247a8417f3eef795da29
Matt Moncayo <matthew.moncayo@gmail.com>
2020-08-22 16:54:21 -0700

    Updates

---
+++ precache-manifest.fffe08f5afa465036e9cd46c0df2eda7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ec7c94de47d0c51f22e3d45b6254fb",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "9969ae172bdbed521e44",
+    "url": "/react-portfolio/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "992935a3d1674d6ac19e",
+    "url": "/react-portfolio/static/css/main.481aa705.chunk.css"
+  },
+  {
+    "revision": "9969ae172bdbed521e44",
+    "url": "/react-portfolio/static/js/2.b4b61087.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.b4b61087.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "992935a3d1674d6ac19e",
+    "url": "/react-portfolio/static/js/main.9f1b55a8.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "067c2c665e1e886a93f64e7c5a6737cb",
+    "url": "/react-portfolio/static/media/bio.067c2c66.jpg"
+  },
+  {
+    "revision": "3eed659984e566810f7980421cbcb8cb",
+    "url": "/react-portfolio/static/media/burger.3eed6599.png"
+  },
+  {
+    "revision": "d684a9148bedbad45a73dc2814812929",
+    "url": "/react-portfolio/static/media/job.d684a914.png"
+  },
+  {
+    "revision": "3654b90fd92e861f1ca83dd6b1c43d48",
+    "url": "/react-portfolio/static/media/loo.3654b90f.png"
+  },
+  {
+    "revision": "96993ad150d7b1c2dd104bcde25f64a6",
+    "url": "/react-portfolio/static/media/planner.96993ad1.png"
+  },
+  {
+    "revision": "182a5535ab0970b59cc8ae2c02ab05da",
+    "url": "/react-portfolio/static/media/quiz.182a5535.png"
+  },
+  {
+    "revision": "ccf0dde652af6b1dde7873714e5793a6",
+    "url": "/react-portfolio/static/media/vault.ccf0dde6.png"
+  }
+]);
